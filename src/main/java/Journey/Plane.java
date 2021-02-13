820aeb7c37b17a1866075015f9cf49149164ce2a
Bela <belahamid@gmail.com>
2021-02-13 19:08:29 +0000

    created Plane enum

--- src/main/java/Journey/Plane.java
+++ src/main/java/Journey/Plane.java
@@ -1,4 +1,24 @@
 package Journey;
 
 public enum Plane {
+    BOEING745(200, 14000),
+    LOCKHEED_L_1011(250,17500),
+    DOUGLAS_DC_10(300, 21000);
+
+    private final int capacity;
+    private final double weight;
+
+    Plane(int capacity, double weight){
+        this.capacity = capacity;
+        this.weight = weight;
+    }
+
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
 }
